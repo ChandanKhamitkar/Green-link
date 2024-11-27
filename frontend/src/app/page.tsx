f9f8a0af3a253fa0d8e4cc5ccecb7fb386534ed5
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2024-11-27 11:13:09 +0530

    Added env + Navbar
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,25 +4,28 @@ import { useRouter } from "next/navigation";
 import { v4 as uuidv4 } from 'uuid';
 import { IoIosVideocam } from "react-icons/io";
 import { MdArrowOutward } from "react-icons/md";
-
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
 
   const router = useRouter();
   const roomID = uuidv4();
 
-  return(
-    <div className="bg-white text-white flex gap-5 justify-center items-center h-screen">
+  return (
+    <div className="bg-white text-white flex flex-col gap-5 justify-center items-center h-screen relative">
+      <Navbar />
       {/* Create Instant Meet Button */}
-      <button onClick={() => router.push(`/sender/${roomID}/host`)} className="flex gap-3 justify-center items-center px-5 py-2 rounded-md bg-blue-500  font-semibold">
-        <span className="size-4"><IoIosVideocam /></span>
-        <span className="text-base">Create Instant Meet</span>
-      </button>
-      {/* Join Now Button */}
-      <button className="flex gap-3 justify-center items-center px-5 py-2 rounded-md bg-green-500 text-black font-semibold">
-        <span className="size-4"><MdArrowOutward /></span>
-        <span className="text-base">Join Now</span>
-      </button>
+      <div className="flex justify-center items-center space-x-4">
+        <button onClick={() => router.push(`/sender/${roomID}/host`)} className="flex gap-3 justify-center items-center px-5 py-2 rounded-md bg-blue-500  font-semibold">
+          <span className="size-4"><IoIosVideocam /></span>
+          <span className="text-base">Create Instant Meet</span>
+        </button>
+        {/* Join Now Button */}
+        <button className="flex gap-3 justify-center items-center px-5 py-2 rounded-md bg-green-500 text-black font-semibold">
+          <span className="size-4"><MdArrowOutward /></span>
+          <span className="text-base">Join Now</span>
+        </button>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,40 @@
-export default function Navbar() {
-    // return(
+'use client';
+import { useEffect, useState } from "react";
 
-    // );
+export default function Navbar() {
+    const [timeSpace, setTimSpace] = useState('');
+
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+    useEffect(() => {
+        const date = new Date();
+        console.log('time =', date.toLocaleTimeString());
+        console.log('day =', date.getDay());
+        console.log('day =', date.getFullYear());
+
+        setTimSpace(`${date.toLocaleTimeString()} . ${daysOfWeek[date.getDay()]} . ${date.getFullYear()} `)
+    }, []);
+    return(
+        <div className="absolute top-0 left-0 w-full bg-neutral-100 shadow-md py-3 px-5 flex justify-between items-center">
+            <div className="flex justify-center items-center space-x-2">
+                <img src="/logo.png" alt="Green link logo" className="size-8"/>
+                <p className="text-2xl font-bold tracking-wide text-black">
+                    GreenLink
+                </p>
+            </div>
+
+            {/* Time */}
+            {
+                timeSpace !== '' && <p className="text-xl font-semibold text-black/60 antialiased tracking-wider ">{timeSpace}</p>
+            }
+            
+        </div>
+    );
 };
