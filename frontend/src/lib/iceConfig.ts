e6eb75cc689b6147c4845b7a617dc8571faae2c5
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2024-12-02 15:46:16 +0530

    Added overrelay credentials
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- frontend/src/lib/iceConfig.ts
+++ frontend/src/lib/iceConfig.ts
@@ -1,37 +1,27 @@
 export const iceConfiguration = {
   iceServers: [
-    // {
-    //   urls: "stun:stun.l.google.com:19302", // Public STUN server
-    // },
     {
-      urls: "turn:numb.viagenie.ca",
-      credential: "muazkh",
-      username: "webrtc@live.com",
+      urls: "stun:stun.relay.metered.ca:80",
     },
     {
-      urls: "turn:192.158.29.39:3478?transport=udp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808",
+      urls: "turn:global.relay.metered.ca:80",
+      username: "a8d2842aad0d3d071366e91c",
+      credential: "s2nOs8Mg3XGy3Wic",
     },
     {
-      urls: "turn:192.158.29.39:3478?transport=tcp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808",
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "a8d2842aad0d3d071366e91c",
+      credential: "s2nOs8Mg3XGy3Wic",
     },
     {
-      urls: "turn:turn.bistri.com:80",
-      credential: "homeo",
-      username: "homeo",
+      urls: "turn:global.relay.metered.ca:443",
+      username: "a8d2842aad0d3d071366e91c",
+      credential: "s2nOs8Mg3XGy3Wic",
     },
     {
-      urls: "turn:turn.anyfirewall.com:443?transport=tcp",
-      credential: "webrtc",
-      username: "webrtc",
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "a8d2842aad0d3d071366e91c",
+      credential: "s2nOs8Mg3XGy3Wic",
     },
-    {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-  ],
+],
 };
