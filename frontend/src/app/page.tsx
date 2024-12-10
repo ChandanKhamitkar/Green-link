ca515eb7a4abec2cf11945b5ce00321984702ea1
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2024-12-10 20:01:04 +0530

    Added Footer
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { IoIosVideocam } from "react-icons/io";
 import { MdArrowOutward } from "react-icons/md";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
 
@@ -15,13 +16,13 @@ export default function Home() {
     <div className="bg-[var(--main-color)] text-white flex flex-col gap-5 justify-center items-center h-screen relative">
       <Navbar />
       {/* Create Instant Meet Button */}
-      <div className="w-screen mobile:h-fit h-screen flex mobile:flex-row flex-col-reverse mobile:justify-between justify-center mobile:items-center items-start px-8 space-y-3 mobile:gap-0 gap-10">
+      <div className="w-screen h-screen flex mobile:flex-row flex-col-reverse mobile:justify-between justify-center mobile:items-center items-start px-8 space-y-3 mobile:gap-0 gap-10">
         <div className="flex flex-col justify-between items-start space-y-3">
           <div className="flex flex-col justify-start space-y-2">
             <h2 className="mobile:text-3xl text-2xl font-bold">
-              Peer-to-Peer Video Calling with WebRTC
+              Peer-to-Peer Video Calling <br /> with WebRTC
             </h2>
-            <p className="mobile:text-base text-sm">A basic demonstration of WebRTC's capabilities, showcasing simple two-way video chat functionality.</p>
+            <p className="mobile:text-base text-sm">A basic demonstration of WebRTC&apos;s capabilities, <br />showcasing simple two-way video chat functionality.</p>
           </div>
           <div className="flex mobile:flex-row flex-col justify-center mobile:items-center items-start mobile:space-x-4 space-x-0 mobile:space-y-0 space-y-4">
             <button onClick={() => router.push(`/sender/${roomID}/host`)} className="flex gap-3 justify-center items-center px-5 py-2 rounded-md bg-blue-500  font-semibold">
@@ -38,6 +39,7 @@ export default function Home() {
 
         <img src="/images/hero.jpg" alt="Hero" className="mobile:w-[30%] w-[50%] rounded-md" />
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <p className="text-center w-screen pb-4 font-mono">
+            Made with ❤️ By Chandan Khamitkar
+        </p>
+    );
+};
